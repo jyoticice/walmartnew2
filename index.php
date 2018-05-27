5cb20bc6d637a2a8e0dadbe78a6c463c0e1a01b9
jyoticice <39135407+jyoticice@users.noreply.github.com>
2018-05-27 06:52:45 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php
-header("Location: https://www.walmart.com/browse/jewelry/promise-rings/3891_540912_1228412");
+header("Location: https://www.walmart.com/ip/Mainstays-Black-Metal-Arm-Futon-with-Full-Size-Mattress-Multiple-Colors/45732681?athcpid=45732681&athpgid=athenaItemPage&athcgid=null&athznid=PWVUB&athieid=v0&athstid=CS002&athguid=466001f5-25fcf6dd-f7352c30b2bbd689&athena=true");
 die();
 ?>
